d3e6f9b03c4ab95d73baaaa65b37b8f4dba9afa8
Kato Hiroki <k.hiro1818@gmail.com>
2022-07-31 05:32:48 +0000

    :art: Enable to open link in seperate tab (#3787)

---
+++ docs/javascripts/link_as_new_tab.js
@@ -0,0 +1,5 @@
+// See:
+// https://blog.takoyama.net/technique/md-link-tab/
+jQuery(function () {
+  jQuery("a[href^='http']:not([href*='" + location.hostname + "'])").attr('target', '_blank').attr('rel', 'noopener');
+});
