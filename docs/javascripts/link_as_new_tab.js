7507f23dadde93dc431239044a4f62c7bc6a10e4
Kato Hiroki <k.hiro1818@gmail.com>
2022-07-31 05:46:18 +0000

    :books: Fix url (#3787)

--- docs/javascripts/link_as_new_tab.js
+++ docs/javascripts/link_as_new_tab.js
@@ -1,5 +1,5 @@
 // See:
-// https://blog.takoyama.net/technique/md-link-tab/
+// https://gist.github.com/okuden-labo/a53927e51187d3951ead
 jQuery(function () {
   jQuery("a[href^='http']:not([href*='" + location.hostname + "'])").attr('target', '_blank').attr('rel', 'noopener');
 });
