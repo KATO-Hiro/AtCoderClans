fa9df29a662fbb5d694355f15dfd10d0ef525f81
Kato Hiroki <k.hiro1818@gmail.com>
2022-09-03 06:03:08 +0000

    :boom: Remove old files (#4538)

--- javascript/link_as_new_tab.js
+++
@@ -1,5 +0,0 @@
-// See:
-// https://blog.takoyama.net/technique/md-link-tab/
-jQuery(function () {
-  jQuery("a[href^='http']:not([href*='" + location.hostname + "'])").attr('target', '_blank');
-});
